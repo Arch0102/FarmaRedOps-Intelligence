5643ee5725514b6eef9ae9e210e16c7298176edc
Santiago Ortiz <151885267+Arch0102@users.noreply.github.com>
2026-05-18 21:40:54 -0500

    chore: initialize clean Spring Boot backend

---
+++ src/main/java/com/farmared/opsintelligence/FarmaRedOpsIntelligenceApplication.java
@@ -0,0 +1,13 @@
+package com.farmared.opsintelligence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaRedOpsIntelligenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FarmaRedOpsIntelligenceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/farmared/opsintelligence/FarmaRedOpsIntelligenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmared.opsintelligence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaRedOpsIntelligenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
